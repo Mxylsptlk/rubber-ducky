876f597668ab11d1cb7e25ce54401f000239ca10
Mxylsptlk <qbralloq@gmail.com>
2026-09-11 17:25:40 -0400

    Adding home box sandbox

---
+++ sandbox/box.py
@@ -0,0 +1,43 @@
+# %%
+import os
+import sys
+from dotenv import load_dotenv
+
+BASE_DIR = '/home/phil/git/rubber-ducky'
+load_dotenv(os.path.join(BASE_DIR,'.env'))
+
+# %%
+
+
+sys.path.append(BASE_DIR + '/rubber_ducky')
+
+from agent_graph import RubberDuckyAgentGraph
+
+# %%
+
+from langchain_ollama import OllamaLLM, OllamaEmbeddings
+
+model = OllamaLLM(model="llama3.2:3b")
+brainstormer = OllamaLLM(model="gemma4:26b")
+problem_solver = OllamaLLM(model="gemma4:26b")
+
+embeddings = OllamaEmbeddings(model='nomic-embed-text')
+
+graph = RubberDuckyAgentGraph(
+    model,
+    embeddings,
+    # brainstorming_model=brainstormer,
+    # problem_solving_model=problem_solver
+)
+
+# %%
+
+result = graph.invoke(
+	("Give me some ideas for finding leads for a solopreneur "
+	  "working in analytics and predictive modeling.")
+)
+
+print(result['response'])
+
+# %%
+
